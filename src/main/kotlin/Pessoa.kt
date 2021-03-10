ced73826f5b4c181838ed2ba7d14ecd1154ce693
Fernando Shibata <shibata.421@gmail.com>
2021-03-10 17:42:36 -0300

    First commit

---
+++ src/main/kotlin/Pessoa.kt
@@ -0,0 +1,11 @@
+class Pessoa {
+    val nome: String = "Fernando"
+    val cpf: String = "111.222.333-45"
+}
+
+fun main() {
+    val fernando = Pessoa()
+
+    println(fernando.nome)
+    println(fernando.cpf)
+}
